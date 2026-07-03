f75971c434d59898119cc17c4c18050b130e93e9
nathee-rua <nathee.rua@thaimooc.ac.th>
2026-07-03 14:51:10 +0700

    Move serverExternalPackages to root of nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverExternalPackages: ['@supabase/supabase-js', '@supabase/ssr'],
-  },
+  serverExternalPackages: ['@supabase/supabase-js', '@supabase/ssr'],
 };
 
 export default nextConfig;
